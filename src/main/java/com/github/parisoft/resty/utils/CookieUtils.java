143635c88f587c0658a99e2b4e6c87bd1bce844e
André Paris <apoliveira@uolinc.com>
2015-03-12 16:51:01 -0300

    utils for cookie transformations to java.lang.String

---
+++ src/main/java/com/github/parisoft/resty/utils/CookieUtils.java
@@ -0,0 +1,110 @@
+package com.github.parisoft.resty.utils;
+
+import static com.github.parisoft.resty.utils.ArrayUtils.isEmpty;
+
+import java.net.HttpCookie;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import javax.ws.rs.core.Cookie;
+
+public class CookieUtils {
+
+    public static String toString(Cookie... cookies) {
+        if (isEmpty(cookies)) {
+            return null;
+        }
+
+        final HttpCookie[] httpCookies = new HttpCookie[cookies.length];
+
+        for (int i = 0; i < cookies.length; i++) {
+            final Cookie srcCookie = cookies[i];
+            final HttpCookie httpCookie = new HttpCookie(srcCookie.getName(), srcCookie.getValue());
+            httpCookie.setDomain(srcCookie.getDomain());
+            httpCookie.setPath(srcCookie.getPath());
+            httpCookie.setVersion(srcCookie.getVersion());
+
+            httpCookies[i] = httpCookie;
+        }
+
+        return toString(httpCookies);
+    }
+
+    public static String toString(org.apache.http.cookie.Cookie... cookies) {
+        if (isEmpty(cookies)) {
+            return null;
+        }
+
+        final HttpCookie[] httpCookies = new HttpCookie[cookies.length];
+
+        for (int i = 0; i < cookies.length; i++) {
+            final org.apache.http.cookie.Cookie srcCookie = cookies[i];
+            final HttpCookie httpCookie = new HttpCookie(srcCookie.getName(), srcCookie.getValue());
+            httpCookie.setComment(srcCookie.getComment());
+            httpCookie.setDomain(srcCookie.getDomain());
+            httpCookie.setPath(srcCookie.getPath());
+            httpCookie.setSecure(srcCookie.isSecure());
+            httpCookie.setVersion(srcCookie.getVersion());
+
+            final Date now = new Date();
+
+            if (srcCookie.isExpired(now)) {
+                httpCookie.setMaxAge(0);
+            } else {
+                httpCookie.setMaxAge(TimeUnit.MILLISECONDS.toSeconds(srcCookie.getExpiryDate().getTime() - now.getTime()));
+            }
+
+            httpCookies[i] = httpCookie;
+        }
+
+        return toString(httpCookies);
+    }
+
+    public static String toString(HttpCookie... cookies) {
+        if (isEmpty(cookies)) {
+            return null;
+        }
+
+        final StringBuilder cookieBuilder = new StringBuilder();
+
+        for (HttpCookie cookie : cookies) {
+            cookieBuilder.append(cookie.getName()).append("=").append(cookie.getValue());
+
+            if (cookie.getComment() != null) {
+                cookieBuilder.append(";").append("Comment=").append(cookie.getComment());
+            }
+
+            if (cookie.getDomain() != null) {
+                cookieBuilder.append(";").append("Domain=").append(cookie.getDomain());
+            }
+
+            if (cookie.getPath() != null) {
+                cookieBuilder.append(";").append("Path=").append(cookie.getPath());
+            }
+
+            if (cookie.getSecure()) {
+                cookieBuilder.append(";").append("Secure");
+            }
+
+            if (cookie.isHttpOnly()) {
+                cookieBuilder.append(";").append("HttpOnly");
+            }
+
+            if (cookie.getVersion() > 0) {
+                cookieBuilder.append(";").append("Version=").append(cookie.getVersion());
+            }
+
+            if (cookie.hasExpired()) {
+                cookieBuilder.append(";").append("Max-Age=0");
+            } else {
+                cookieBuilder.append(";").append("Max-Age=").append(cookie.getMaxAge());
+            }
+
+            cookieBuilder.append(", ");
+        }
+
+        return cookieBuilder
+                .deleteCharAt(cookieBuilder.length() - 1)
+                .toString();
+    }
+}
