bf1f68e1580ec55e288826588894593bb5ee7664
André Paris <apoliveira@uolinc.com>
2015-03-13 14:03:12 -0300

    correct version on tuto

--- src/main/java/com/github/parisoft/resty/response/Response.java
+++ src/main/java/com/github/parisoft/resty/response/Response.java
@@ -5,11 +5,6 @@ import org.apache.http.HttpResponse;
 import com.github.parisoft.resty.entity.EntityReader;
 import com.github.parisoft.resty.response.http.HttpResponseExtension;
 
-/**
- * {@inheritDoc HttpResponse}
- * @author apoliveira
- *
- */
 public interface Response extends HttpResponse, HttpResponseExtension, EntityReader {
 
 }
